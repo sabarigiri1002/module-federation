fea0a4b0bacfbb0838c27abbd09341dca46cc388
Sabari <sabari@Sabari-MacBook-Pro.local>
2023-03-06 13:57:47 +0530

    module-federation initial setup

---
+++ home/src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+    return (
+        <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+            Only the Best Spinners
+        </div>
+    );
+}
+
+export default Footer;
